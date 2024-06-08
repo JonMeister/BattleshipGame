a102f5598ca213a77717ce056c47d18ad8018cb8
Jonathan Aristizabal <jonathan.aristizabal@correounivalle.edu.co>
2024-06-07 20:12:35 -0500

    Game logic finished

--- src/main/java/com/jonathan/battleshipgame/model/Board.java
+++ src/main/java/com/jonathan/battleshipgame/model/Board.java
@@ -1,35 +1,55 @@
 package com.jonathan.battleshipgame.model;
 
-import javafx.geometry.Point2D;
+import com.jonathan.battleshipgame.exceptions.CellAlreadyAttackedException;
+import com.jonathan.battleshipgame.exceptions.GameOverException;
 import javafx.geometry.Pos;
-import javafx.scene.Group;
 import javafx.scene.control.TextField;
-import javafx.scene.input.ClipboardContent;
-import javafx.scene.input.DragEvent;
-import javafx.scene.input.Dragboard;
-import javafx.scene.input.TransferMode;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Board {
 
-    private boolean[][] myShipsPositions = new boolean[10][10];
-    private boolean [][] enemyShipsPositions = new boolean[10][10];
-    List <boolean[][]> enemyGames= new ArrayList<boolean[][]>();
+    private int[][] myShipsPositions = new int[10][10];
+    private int[][] enemyShipsPositions = new int[10][10];
+    private int[][] hitsOnEnemyShips = new int[10][10];
+    private int[][] hitsOnMyShips = new int[10][10];
+    private IBoard boardListener;
+
+    public void setBoardListener(IBoard boardListener) {
+        this.boardListener = boardListener;
+    }
+
+    private Image fail = new Image(String.valueOf(getClass().getResource("/com/jonathan/battleshipgame/images/x.png")));
+    private Image hit = new Image(String.valueOf(getClass().getResource("/com/jonathan/battleshipgame/images/bomb.png")));
+    private Image sunk = new Image(String.valueOf(getClass().getResource("/com/jonathan/battleshipgame/images/fire.png")));
+    private Set<String> attackedCells = new HashSet<>();
+    private Random random = new Random();
+
+
+    List<int[][]> enemyGames = new ArrayList<>();
+    private int onFire;
 
     private GridPane myBoard;
     private GridPane enemyBoard;
-
     private GridPane shipsBoard;
-    private boolean isRotated = false;
-    private boolean isVertical;
 
-    public Board(GridPane myBoard, GridPane shipsBoard,GridPane enemyBoard) {
+    public Board(GridPane myBoard, GridPane shipsBoard, GridPane enemyBoard) {
         this.myBoard = myBoard;
         this.shipsBoard = shipsBoard;
-        this.enemyBoard= enemyBoard;
+        this.enemyBoard = enemyBoard;
+        resetBoard();
+    }
+    private void resetBoard() {
+        // Reinicializar las matrices
+        myShipsPositions = new int[10][10];
+        enemyShipsPositions = new int[10][10];
+        hitsOnEnemyShips = new int[10][10];
+        hitsOnMyShips = new int[10][10];
+
+        attackedCells.clear(); // Limpiar las celdas atacadas
     }
 
     public void populateGrid() {
@@ -57,75 +77,233 @@ public class Board {
         textField.setMouseTransparent(true);
         return textField;
     }
-    public void markBoardOccupied(int row, int col, int length, boolean isVertical) {
-        if(isVertical){
-            for(int i=0;i<length;i++){
-                if(row+i<10){
-                    myShipsPositions[row+i][col]=true;//Marcarcon2siesvertical
+
+    public void markBoardOccupied(int row, int col, int length, boolean isVertical,int num) {
+        if (isVertical) {
+            for (int i = 0; i < length; i++) {
+                if (row + i < 10) {
+                    myShipsPositions[row + i][col] = num;
                 }
             }
-        }else{
-            for(int i=0;i<length;i++){
-                if(col+i<10){
-                    myShipsPositions[row][col+i]=true;//Marcarcon1sieshorizontal
+        } else {
+            for (int i = 0; i < length; i++) {
+                if (col + i < 10) {
+                    myShipsPositions[row][col + i] = num;
                 }
             }
         }
-
     }
 
-    public boolean[][] enemyShipsPositions(int position){
-        boolean[][] matrix1 = {
-                {false, false, false, false, false, false, false, false, false, false},
-                {false, true,  true,  true,  true,  false, false, false, false, false},
-                {false, false, false, false, false, false, false, false, false, false},
-                {false, false, false, true,  true,  true,  false, false, false, false},
-                {false, false, false, false, false, false, false, false, false, false},
-                {false, false, false, false, false, false, false, false, false, false},
-                {false, false, false, false, false, false, true,  true,  false, false},
-                {false, false, false, false, false, false, false, false, false, false},
-                {false, true,  false, false, false, false, false, false, false, false},
-                {false, false, false, false, true,  false, false, false, false, false}
+    public void selectGame(int position) {
+        int[][] matrix1 = {
+                {0, 0, 0, 0, 0, 0, 0, 3, 3, 3},
+                {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 0, 10, 0, 0, 4, 4},
+                {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 2, 2, 2, 0, 0, 9, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 5, 0, 0, 7, 0, 0, 6, 0, 0},
+                {0, 5, 0, 0, 0, 0, 0, 6, 0, 0},
+                {0, 0, 0, 8, 0, 0, 0, 0, 0, 0}
         };
-        boolean[][] matrix2 = {
-                {false, false, false, false, false, false, false, false, false, false},
-                {false, false, false, false, true,  true,  false, false, false, false},
-                {false, true,  true,  true,  false, false, false, false, false, false},
-                {false, false, false, false, false, false, false, false, false, false},
-                {false, false, false, false, false, false, false, false, false, false},
-                {true,  true,  true,  true,  false, false, false, false, false, false},
-                {false, false, false, false, false, false, false, false, false, false},
-                {false, false, true,  true,  false, false, false, false, false, false},
-                {false, false, false, false, false, false, false, false, false, false},
-                {false, true,  false, false, false, true,  false, false, false, false}
+        int[][] matrix2 = {
+                {0, 0, 0, 0, 0, 10, 0, 0, 0, 4},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 4},
+                {6, 6, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 3, 0, 0, 7, 0, 0},
+                {0, 0, 0, 0, 3, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 3, 0, 0, 0, 0, 0},
+                {0, 8, 0, 9, 0, 0, 5, 5, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 2, 2, 2, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 1, 1, 1, 1}
         };
-        boolean[][] matrix3 = {
-                {false, false, false, false, false, false, false, false, false, false},
-                {false, false, false, false, false, false, false, false, false, false},
-                {false, false, false, false, false, false, false, false, true,  false},
-                {false, true,  true,  true,  true,  false, false, false, false, false},
-                {false, false, false, false, false, false, false, false, false, false},
-                {true,  true,  true,  false, false, false, false, false, false, false},
-                {false, false, false, false, false, true,  true,  false, false, false},
-                {false, false, false, false, false, false, false, false, false, false},
-                {false, false, false, false, true,  true,  false, false, false, false},
-                {false, false, false, false, false, false, true,  false, false, false}
+        int[][] matrix3 = {
+                {3, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {3, 0, 0, 7, 0, 0, 6, 6, 0, 0},
+                {3, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 1, 0, 0, 9, 0, 0},
+                {0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+                {0, 8, 0, 0, 1, 0, 0, 0, 4, 4},
+                {0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+                {0, 5, 5, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 2, 2, 2, 0},
+                {0, 0, 10, 0, 0, 0, 0, 0, 0, 0}
         };
         enemyGames.add(matrix1);
         enemyGames.add(matrix2);
         enemyGames.add(matrix3);
-        return enemyGames.get(position);
+        enemyShipsPositions = enemyGames.get(position);
+        hitsOnEnemyShips = new int[10][10];
+        for (int i = 0; i < 10; i++) {
+            System.arraycopy(enemyShipsPositions[i], 0, hitsOnEnemyShips[i], 0, 10);
+        }
     }
 
-    public void attack
+    public ImageView attackEnemyBoard(int row, int col) throws CellAlreadyAttackedException, GameOverException {
+        ImageView imageView = null;
+        int damaged;
+        if (hitsOnEnemyShips[row][col] == -1) {
+            throw new CellAlreadyAttackedException("Esta celda ya ha sido atacada");
+        }
+        hitsOnEnemyShips[row][col] = -1;
+        if (enemyShipsPositions[row][col] != 0) {
+            damaged = enemyShipsPositions[row][col];
+
+            boolean isSunk = true;
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
+                    if (hitsOnEnemyShips[i][j] == damaged) {
+                        isSunk = false;
+                        break;
+                    }
+                }
+                if (!isSunk) break;
+            }
+
+            if (isSunk) {
+                imageView = new ImageView(sunk);
+                this.onFire = damaged;
+                if (checkGameOver()) {
+                    throw new GameOverException("All enemy ships have been sunk!");
+                }
+            } else {
+                imageView = new ImageView(hit);
+                this.onFire = 0;
+            }
+        } else {
+            imageView = new ImageView(fail);
+            this.onFire = 0;
+        }
+
+        // Ensure the image fits within the cell size (optional)
+        imageView.setFitWidth(30);
+        imageView.setFitHeight(30);
+
+        // Add the image to the enemyBoard GridPane
+        return imageView;
+    }
+
+    public int isOnFire() {
+        return onFire;
+    }
+
+    public int[][] getEnemyShipsPositions() {
+        return enemyShipsPositions;
+    }
+    public int[][] getMyShipsPositions() {
+        return myShipsPositions;
+    }
+
+    public ImageView fireImage() {
+        ImageView imageView = new ImageView(sunk);
+        imageView.setFitWidth(30);
+        imageView.setFitHeight(30);
+        return imageView;
+    }
 
     public void printBoard() {
-        for (boolean[] myShipsPosition : myShipsPositions) {
-            for (boolean b : myShipsPosition) {
-                System.out.print(b ? "1" : "0");
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print(hitsOnMyShips[i][j] + " ");
             }
             System.out.println();
         }
     }
+
+    private boolean checkGameOver() {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (hitsOnEnemyShips[i][j] > 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public ImageView attackMyBoard() throws GameOverException {
+        int row, col;
+        String cell;
+
+        // Buscar una celda no atacada
+        do {
+            row = random.nextInt(10);
+            col = random.nextInt(10);
+            cell = row + "," + col;
+        } while (attackedCells.contains(cell));
+
+        // Marcar la celda como atacada
+        attackedCells.add(cell);
+        hitsOnMyShips[row][col] = -1;
+        boolean hit = myShipsPositions[row][col] != 0;
+        boolean sunk = false;
+
+        // Determinar el resultado del ataque
+        if (hit) {
+            int damaged = myShipsPositions[row][col];
+            boolean isSunk = true;
+
+            // Verificar si el barco está hundido
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
+                    if (myShipsPositions[i][j] == damaged && hitsOnMyShips[i][j] != -1) {
+                        isSunk = false;
+                        break;
+                    }
+                }
+                if (!isSunk) break;
+            }
+
+            if (isSunk) {
+                sunk = true;
+                for (int i = 0; i < 10; i++) {
+                    for (int j = 0; j < 10; j++) {
+                        if (myShipsPositions[i][j] == damaged) {
+                            hitsOnMyShips[i][j] = -1; // Asegurar que se marque como atacada
+                            if (boardListener != null) {
+                                boardListener.onCellAttacked(i, j, true, true);
+                            }
+                        }
+                    }
+                }
+                // Verificar si todos los barcos han sido hundidos
+                if (checkMyBoardGameOver()) {
+                    if (boardListener != null) {
+                        boardListener.onGameOver();
+                    }
+                    throw new GameOverException("All your ships have been sunk!");
+                }
+            }
+        }
+
+        // Notificar al oyente sobre el resultado del ataque
+        if (boardListener != null) {
+            boardListener.onCellAttacked(row, col, hit, sunk);
+        }
+
+        return new ImageView(); // Retorna una imagen adecuada según la lógica de la vista
+    }
+
+    private boolean checkMyBoardGameOver() {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (myShipsPositions[i][j] != 0 && hitsOnMyShips[i][j] != -1) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void setHitsOnMyShips(){
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                this.hitsOnMyShips[i][j] = myShipsPositions[i][j];
+            }
+        }
+    }
+
 
 }
